0ee4f2c3679ffe02a4a188d4a3610bb40108af65
Joaquin Jimenez Garcia <jgarcia@inpost.pl>
2024-04-02 14:12:11 +0200

    Added debugging file and kwargs

--- functions/kwargs.py
+++ functions/kwargs.py
@@ -1,5 +1,6 @@
 def get_product(**data): # Needs ** to get any kind of data, when called it needs the name of the param and returns JSON.
-    print(data["name"])
     print(data)
     
-get_product(id="5", name="iPhone", desc="iPhone 15 Pro")
+    return data["name"]
+    
+print(get_product(id="5", name="iPhone", desc="iPhone 15 Pro"))
