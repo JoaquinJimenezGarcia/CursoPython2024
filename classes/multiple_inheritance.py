6dc85e0830f7f646065e34504d5db05ac0bb1bcf
Joaquin Jimenez Garcia <garcia.jjimenez@gmail.com>
2024-07-21 23:06:49 +0200

    added comment to multi inheritance

--- classes/multiple_inheritance.py
+++ classes/multiple_inheritance.py
@@ -6,6 +6,8 @@ class Dog:
     def walk(self):
         print("walking...")
     
+# Python starts inheriting by right side to left side. So if the methods are
+# called the same, only the ones in the first position will stay
 class Superdog(Dog,Animal):
     def code(self):
         print("I can code too!")
